3765ed586782a1bf2e73638b57e7f443d73db5a9
Varosu <93037979+Varosu@users.noreply.github.com>
2021-12-21 00:56:12 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,429 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Sourav Portfolio</title>
+  <link rel="shortcut icon" type="image/png" href="images/Logopit_1624442136543.png">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <script src="bootstrap/js/jquery-3.4.1.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>
+  <link href="fontawesome-free-5.15.3-web/css/all.min.css" rel="stylesheet">
+  <link href="css/index.css" rel="stylesheet">
+</head>
+<body>
+   <hr style="width:200px" class="opacity" id="home">
+<br>
+<div class="container" style="margin-top:200px;">
+<div class="row" style="width:100%;height:100vh;">
+    <div class="col-sm-4">
+       <div class="my_img">
+            <img src="images/IMG_20210116_103810~2.jpg">
+          </div>
+
+    </div>
+    <div class="col-sm-4">
+         <div class="text">
+       <h2>HI I'M SOURAV MUKHERJEE</h2>
+          <p>Web desinge & Photographey</p>
+          </div>
+    </div>
+</div>
+</div>
+<br>
+<hr style="width:200px; opacity:0;"  id="about">
+    <br>
+<br>
+<div class="container"  style="margin-top:50px;">
+  <div class="row">
+    <div class="col-sm-4">
+      <h2 style="color:yellow;"><span>A</span><span>b</span><span>o</span><span>u</span><span>t</span><span> </span><span> M</span><span>e</span></h2>
+      <h5>Photo of Me:</h5>
+      <div class="fakeimg"><img src="images/original_f417f16e-755d-43dd-aca6-b8eeb000307a_IMG_20210124_094837~2.jpg"></div>
+      <p>Sourav Mukherjee (23)</p>
+      <h2>Some social media Links</h3>
+      <p>Follow our social media pages.</p>
+      <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Active<i class="fas fa-circle"></i> </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="https://www.facebook.com/groups/376485529831099/"><i class="fab fa-facebook-f"></i></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="mailto:mukherjeesourav233@gmail.com"> <i class="fas fa-envelope"></i> </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled"id="whatsapp" href="whatsapp:7044718465"> <i class="fab fa-whatsapp-square"></i></a>
+        </li>
+      </ul>
+      <hr class="d-sm-none">
+	  <h5 style="color:yellow;"><i class="fas fa-address-card"></i> :-</h5>
+      <div class=""><center><p><b>Station road Moynadana ,Hooghly-Chinsurah, West Bengal, India, pin-712102</b></p></center></div>
+      <br> </p>
+	     <h5 style="color:#157CF3;"><i class="fas fa-phone-square-alt"></i> :-</h5>
+      <div class=""><center><p><b><a href="call:8013278673;">8013278673</a>
+<a href="whatsapp:8013278673;">whatsapp- 8013278673</a></b></p></center></div>
+      <p></p>
+<br>
+	  <h5 style="color:#157CF3"><b><i class="fas fa-envelope-square"></i> :-</b> </h5>
+      <div class=""><center><p><b> <a href="mailto:mukherjeesourav233@gmail.com"> mukherjeesourav233@gmail.com</a> </b></p></center></div>
+
+    </div>
+    <div class="col-sm-8" >
+	<h5 style="color:yellow;"><u>Over View</u></h5>
+	<Center>
+<div class="accordion" id="accordionExample">
+	<div class="card">
+	<div class="card-header" id="headingOne">
+	<h2 class="mb-0">
+	<button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+	<h5 style="color:white;">Skills & Languages<h5>
+	</button>
+	</h2>
+	</div>
+	<div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+	<div class="card-body">
+<h3>SKILLS</h3>
+	<p>photoshop</p>
+	<br>
+	<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="50"
+  aria-valuemin="0" aria-valuemax="100" style="width:50%">
+    50%
+  </div>
+</div><br>
+<p>HTML</p>
+	<br>
+	<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="90"
+  aria-valuemin="0" aria-valuemax="100" style="width:90%">
+    90%
+  </div>
+</div>
+<p>CSS</p>
+	<br>
+	<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="50"
+  aria-valuemin="0" aria-valuemax="100" style="width:50%">
+    50%
+  </div>
+</div>
+<p>JAVA SCRIPT</p>
+	<br>
+	<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="50"
+  aria-valuemin="0" aria-valuemax="100" style="width:50%">
+    50%
+  </div>
+</div>
+<p>PHP</p>
+	<br>
+	<div class="progress">
+  <div class="progress-bar" role="progressbar" aria-valuenow="50"
+  aria-valuemin="0" aria-valuemax="100" style="width:50%">
+    50%
+  </div>
+</div>
+<h3>LANGUAGES</h3>
+<P>BENGALI , HINDI , ENGLISH</P>
+	</div>
+	</div>
+	</div>
+	<div class="card">
+	<div class="card-header" id="headingTwo">
+	<h2 class="mb-0">
+	<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+	<h5 style="color:white;">Education<h5>
+	</button>
+	</h2>
+	</div>
+	<div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+	<div class="card-body">
+	<h3> SECONDARY:-</h3><P>2014 SCHOOL- Khalisani Vidyamandir</P><br>
+	<h3>HIGHER SECONDARY:-</h3><P>2016 SCHOOL-Dr. Sital Prosad Ghosh Adarsha Sikhalaya(commerce subject-ACCT , BSTD , CSTX , ECON)</P><br>
+	<h3> GRADUATION:-</h3><P>2017 COLLEGE- Hooghly Mohsin College(B.Com. 1styear)</P>
+	</div>
+	</div>
+	</div>
+	<div class="card">
+	<div class="card-header" id="headingThree">
+	<h2 class="mb-0">
+	<button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+	<h5 style="color:white;">Interest & Additional Activities<h5>
+	</button>
+	</h2>
+	</div>
+	<div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+	<div class="card-body">
+	<h3> INTEREST:-</h3><P>Music,drawing,photography</P><br>
+	<h3>ADDITIONAL:-</h3><P>I’m sourav. I’m a web designer living in Chinsurah, West Bengal, India. I am a fan of design, music, and programming. I’m also interested in web development and painting. You can hire me with a click on the button above.
+I am able to handle multiple tasks on a daily basis.
+& use a creative approach to problem solve.
+I am a dependable person who is great at time management.
+I am always energetic and eager to learn new skills.
+I have experience working as part of a team and individually.
+I am flexible in my working hours, being able to work evenings and weekends.
+I am hardworking and always the last to leave the office in the evening.
+I have organised the staff functions for the last four years with great success.
+I am honest and trustworthy when I am counting money after our church bake-sales.
+I work with the team to provide imaginative solutions for our customers.
+I am always coming up with innovative ideas.	</P>
+	</div>
+	</div>
+	</Center>
+<p></p><br><center>
+<h2 style="text-align:center;color:yellow;border:1px solid blue;background-color:black;width:300px;border-radius:10%;"><a href="Mocktale/mocktalepdf/sbio.pdf" style="text-decoration:none;"> <i class="fas fa-cloud-download-alt"></i> DOWNLOAD RESUME </a></h2></center>
+	  <h2 style="color:yellow;"><span>D</span><span>e</span><span>S</span><span>i</span><span>G</span><span>n</span></h2>
+      <h5><u>by me-</u></h5><center><br><h5 style="color:#157CF3;font-size:18px;"><caption><b>MOCKTALE <i class="fas fa-level-down-alt"></i></b></caption></h5></center>
+      <div class="fakeimg"style="border:1px solid #157CF3;"><a href="Mocktale/index.html"><img src="images/screenshot-localhost-2021.06.25-05_08_11.png"></a></div>
+      <br> <center><h5 style="color:#157CF3;font-size:18px;"><caption><b>store<i class="fas fa-level-down-alt"></i></b></caption></h5></center> <div class="fakeimg"style="border:1px solid blue;"><a href="store/index.html"><img src="images/screenshot--2021.06.25-11_37_48.png"></a></div>
+       <br><center><h5 style="color:#157CF3;font-size:18px;"><caption><b>COMMING SOON <i class="fas fa-level-down-alt"></i></b></caption></h5></center> <div class="fakeimg"style="border:1px solid blue;"><a href="#"><img src="images/26690.jpg"></a></div>
+  </div>
+</div>
+</div>
+<br>
+<br>
+<script type="text/javascript">
+  $(function () {
+$('#whatsapp').WhatsApp({
+    phone: '7044718465',
+});
+  });
+</script>
+<br>
+
+<hr style="width:200px" class="opacity" id="serveces">
+<br>
+<br>
+<div class="container"  style="margin-top:50px;">
+     <h2 style="color:yellow;"><span>S</span><span>E</span><span>R</span><span>V</span><span>I</span><span>E</span><span>S</span><span>C</span><span>S</span></h2>
+  <div class="row">
+    <div class="col-sm-4">
+<div class="flip-box">
+  <div class="flip-box-inner">
+    <div class="flip-box-front">
+    <center>
+      <h2 style="color:#157CF3;padding:20px;"><i class="fas fa-palette fa-3x"></i><br>Graphic Designing</h2>
+    </div>
+    <div class="flip-box-back">
+     <h2 style="color:white;padding:20px;"> <i class="fas fa-bezier-curve fa-3x"></i><br>Graphic Designing</h2>
+      </center>
+    </div>
+  </div>
+</div>
+</div>
+<div class="col-sm-4">
+<div class="flip-box">
+  <div class="flip-box-inner">
+    <div class="flip-box-front">
+      <h2 style="color:#157CF3;padding:20px;"><i class="fas fa-laptop-code fa-3x"></i><br>Web Designing</h2>
+    </div>
+    <div class="flip-box-back">
+     <h2 style="color:white;padding:20px;"> <i class="fas fa-mobile fa-3x"></i><br>Responsive</h2>
+    </div>
+  </div>
+</div>
+</div>
+<div class="col-sm-4">
+<div class="flip-box">
+  <div class="flip-box-inner">
+    <div class="flip-box-front">
+      <h2 style="color:#157CF3;padding:20px;"> <i class="fas fa-database fa-3x"></i><br>Database works</h2>
+    </div>
+    <div class="flip-box-back">
+     <h2 style="color:white;padding:20px;"> <i class="far fa-file-code fa-3x"></i></i><br>PHP</h2>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+</div>
+<br>
+<br>
+
+    <hr style="width:200px" class="opacity" id="contact">
+    <br>
+<br>
+  <!-- Contact Section -->
+  <div class="container"id="contact"style="margin-top:50px;">
+         <h2 style="color:yellow;"><span>C</span><span>o</span><span>n</span><span>t</span><span>a</span><span>c</span><span>t</span><span> </span><span> M</span><span>e</span></h2>
+
+
+    <div class="section">
+      <p><i class="fa fa-map-marker"></i> West Bengal,India</p>
+      <p><i class="fa fa-phone"></i> Phone: +918013278673 </p>
+      <p><i class="fa fa-envelope"> </i> Email: mukherjeesourav233@gmail.com</p>
+    </div><br>
+    <p>Let's get in touch. Send me a message:</p>
+
+    <form action="/action_page.php" target="_blank">
+      <p><input class="input padding-16" type="text" placeholder="Name" required name="Name"></p>
+      <p><input class="input padding-16" type="text" placeholder="Email" required name="Email"></p>
+      <p><input class="input padding-16" type="text" placeholder="Subject" required name="Subject"></p>
+      <p><input class="input padding-16" type="text" placeholder="Message" required name="Message"></p>
+      <p>
+        <button class="button" type="submit">
+          <i class="fa fa-paper-plane"></i> SEND MESSAGE
+        </button>
+      </p>
+    </form>
+  <!-- End Contact Section -->
+  </div>
+<hr style="width:200px; opacity:0;"  id="about">
+    <br>
+<br>
+
+<div class="jumbotron text-center" style="margin-bottom:0;background-color:#2c2c2e;">
+<h2 style="float:left; color:white;"><u>Follow our social media Links</u></h2><br>
+<div class="container">
+<div class="row">
+    <div class="col-6">
+         <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="https://www.facebook.com/sourav.mukherjee.92167789/"><span><i class="fab fa-facebook-f"></i></span> <span><u><p>Facebook</p></u></span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="mailto:mukherjeesourav233@gmail.com"><span><i class="fas fa-envelope"></i> <span> <u><p>Gmail</p></u>  </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link "id="whatsapp" href="whatsapp:8013278673"> <span> <i class="fab fa-whatsapp-square"></i></span> <span> <u><p>Whatsapp</p></u></span> </a>
+        </li>
+      </ul>
+       </div>
+
+
+    <div class="col-6">
+         <ul class="nav nav-pills flex-column">
+        <li class="nav-item">
+            <a class="nav-link" href="https://twitter.com/SouravM68067597"><span><i class="fab fa-twitter"></i></span>  <span><u><p> Twitter</p></u></span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link " href="dashboard.html"><span> <i class="fa fa-check"></i></span> <span><u><p> Check our services</p></u></span></a>
+        </li>
+      </ul>
+        </div>
+  </div>
+</div>
+<br>
+  <p style="color:black;">Copyright © Souravportfolio.com</p>
+</div>
+
+
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+ <div class="container">
+    <div class="row">
+        <div class="col-6">
+  <a class="navbar-brand" href="#home"> <div class="logo"><img src="images/Logopit_1624442136543.png"style="width:50px;"></div></a>
+  </div>
+<div class="col-6"  style="float:right;">
+
+  <button class="navbar-toggler" type="button"style="float:right;" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#home"> <span> <i class="fa fa-home fa-1x"></i></span><span><p>home</p></span> </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#about"><span><i class="fa fa-user fa-1x"></i></span><span><p>about</p></span></a>
+      </li>
+	   <li class="nav-item">
+        <a class="nav-link" href="#serveces"> <span><i class="fab fa-servicestack fa-1x"></i></span><span><p>services</p></span></a>
+      </li>
+	  <li class="nav-item">
+        <a class="nav-link" href="#contact"> <span><i class="fas fa-id-badge fa-1x"></i></span><span><p>contact</p></span></a>
+      </li>
+	   <div class="dropdown diactivate">
+    <button class="btn btn-default dropdown-toggle" type="button" id="menu1" data-toggle="dropdown">Members
+    <span class="caret"></span></button>
+    <ul class="dropdown-menu" role="menu" aria-labelledby="menu1">
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#"> <center><span> <i class="fas fa-user-plus"></i>  </span><span><h5>SingUp</h5></span></center></a>
+<div class="h-s">
+<form action="register.php" method="POST"   style="border:1px solid #ccc">
+  <div class="container" style="color:black;" id="sing">
+    <h5>Sign Up</h5>
+    <p>Please fill in this form to create an account.</p>
+    <hr>
+    <label for="username"><b>User Name</b></label>
+    <input type="text" placeholder="Enter User Name" name="name" required>
+
+    <label for="email"><b>Email</b></label>
+    <input type="text" placeholder="Enter Email" name="email" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <label for="psw-repeat"><b>Repeat Password</b></label>
+    <input type="password" placeholder="Repeat Password" name="pswrepeat" required>
+
+      <label for="Address"><b>Address</b></label>
+    <input type="text" placeholder="Enter Address" name="Address" required>
+
+      <label for="phonenumber"><b>Phone Number</b></label>
+    <input type="text" placeholder="Enter PhoneNumber" name="PhoneNumber" required>
+
+    <label>
+      <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+    </label>
+ <button type="submit" name="submit" class="signupbtn" style="width:100%;"> Sign Up</button>
+    <p>By creating an account you agree to our <a href="terms.html" style="color:dodgerblue">Terms & Privacy</a>.</p>
+    <br>
+    <br>
+    <br>
+  </div>
+  </form></div>
+</li>
+
+      <li role="presentation"><a role="menuitem" tabindex="-1" href="#"><center><span> <i class="far fa-user"></i></span><span><h5> Login</h5></span></center></a>
+
+      <div class="h-s">
+     <form action="" method="POST"   style="border:1px solid #ccc; color:black;">
+ <div class="container" style="color:black;">
+ <h5>Login</h5>
+ <p>Please fill in this form to lonin your account.</p>
+ <hr>
+
+
+ <label for="email"><b>Email</b></label>
+ <input type="text" placeholder="Enter Email" name="email" required>
+
+ <label for="psw"><b>Password</b></label>
+ <input type="password" placeholder="Enter Password" name="psw" required>
+
+ <label>
+ <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
+ </label>
+ <button type="submit"name="submit"> Login</button>
+ <p>By creating an account you agree to our <a href="terms.html" style="color:dodgerblue">Terms & Privacy</a>.</p>
+ <br>
+ <br>
+ <br>
+ </div>
+ </form>
+
+
+
+
+
+
+
+
+
+</div>
+      </li></ul>
+    </ul>
+  </div>
+</div>
+</div>
+</div>
+</nav>
+
+
+
+
+</body>
+</html>
